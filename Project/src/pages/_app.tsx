739f6e1fd906a4f60fc2f90fce9abffa7dabde2b
unknown <bach8616@gmail.com>
2023-03-28 09:43:09 +0300

    debug build

--- Project/src/pages/_app.tsx
+++ Project/src/pages/_app.tsx
@@ -1,10 +1,9 @@
 import { Layout } from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
-import { Provider, useDispatch } from "react-redux";
+
+import { Provider } from "react-redux";
 import store from "../../Store/Redux-Store/store";
-import { addTask } from "../../Store/Redux-Store/tasksSlice";
 
 export let rendered = null;
 
--- Project/utils/react/pick.tsx
+++ Project/utils/react/pick.tsx
@@ -1,4 +1,4 @@
-function pick<T extends HTMLElement>() {
+export function pick<T extends HTMLElement>() {
   return <K extends keyof T>(key: K) =>
     <E extends (t: T[K]) => void>(fn: E) =>
     (e: React.SyntheticEvent<T>) =>
